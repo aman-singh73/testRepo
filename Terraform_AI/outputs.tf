a24d136ba02770cf334f1ae0e19c67fa371a7596
Aman kumar singh <134715373+aman-singh73@users.noreply.github.com>
2026-06-12 17:55:37 +0530

    updated outputs.tf

--- Terraform_AI/outputs.tf
+++ Terraform_AI/outputs.tf
@@ -1,22 +1,6 @@
 # Outputs for Terraform configuration
 # Environment: dev
 
-output "app_service_plan_id" {
-  description = "App Service Plan ID"
-  value       = module.shared_plan.id
-}
-
-output "database_connection_string" {
-  description = "Connection string for database"
-  sensitive   = true
-  value       = module.database_cosmos.connection_strings[0]
-}
-
-output "database_endpoint" {
-  description = "Endpoint for database"
-  value       = module.database_cosmos.endpoint
-}
-
 output "log_analytics_workspace_id" {
   description = "Log Analytics Workspace ID"
   value       = module.log_analytics.id
@@ -35,16 +19,6 @@ output "resource_group_id" {
 output "resource_group_name" {
   description = "Resource group name"
   value       = module.main_rg.name
-}
-
-output "user-api_id" {
-  description = "Resource ID of user-api"
-  value       = module.user_api_app.id
-}
-
-output "user-api_url" {
-  description = "URL of user-api"
-  value       = module.user_api_app.default_hostname
 }
 
 output "perf_test_vm_public_ip" {
